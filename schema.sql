2d5d7326e5633fdbc3cdaf92f487d1cd8b528905
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-10-19 00:30:28 +0000

    create species colums in animals table
    alter table with new data
    
    make transactions

--- schema.sql
+++ schema.sql
@@ -5,3 +5,5 @@ CREATE DATABASE vet_clinic;
 CREATE TABLE animals (id INT, name VARCHAR(100),
                                    date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL, PRIMARY KEY(id));
 
+
+ALTER TABLE animals ADD species VARCHAR(100);
