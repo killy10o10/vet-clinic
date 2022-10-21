55e133a280b162a84452a2b04dcfcddba1ad391a
killy10o10 <89058561+killy10o10@users.noreply.github.com>
2022-10-21 17:52:25 +0000

    add relationships
    query multiple data

--- schema.sql
+++ schema.sql
@@ -30,3 +30,22 @@ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 
 
 ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
+
+
+CREATE TABLE vets(id SERIAL NOT NULL PRIMARY KEY,
+                                             name VARCHAR(100),
+                                                  age INT, date_of_graduation DATE);
+
+
+CREATE TABLE specializations (vet_id INT REFERENCES vets(id),
+                                                    species_id INT REFERENCES species(id),
+                                                                              PRIMARY KEY (vet_id,
+                                                                                           species_id));
+
+
+CREATE TABLE visits (animal_id INT REFERENCES animals(id),
+                                              vet_id INT REFERENCES vets(id),
+                                                                    date_of_visit DATE, PRIMARY KEY (animal_id,
+                                                                                                     vet_id,
+                                                                                                     date_of_visit));
+
